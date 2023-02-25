1d3e28e6d8b6834f914c83f886247d58ca76b532
Akash Kandasamy <akashram953@gmail.com>
2023-02-25 12:14:00 +0530

    Tailwind Config - Plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+const postcss = require('postcss')
 module.exports = {
+	darkMode: 'class',
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,svelte}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				inter: ['Inter', 'sans-serif'],
+				rubik: ['Rubik', 'sans-serif'],
+			},
 			spacing: {
 				'5p': '5%',
 				'8p': '8%',
@@ -12,5 +20,20 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ addComponents }) {
+			addComponents({
+				'.center': {
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+				},
+				'.heading': {
+					fontFamily: 'Rubik, sans-serif',
+					fontSize: '1.25rem',
+					fontWeight: 'bold',
+				},
+			})
+		}),
+	],
 }
