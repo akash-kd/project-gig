ec5deeaa014650396791240bf158729933f061ab
Akash Kandasamy <akashram953@gmail.com>
2023-02-27 11:49:38 +0530

    Main

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,53 @@
 <script>
+	// Components
+	import Navbar from './components/Nav/index.svelte'
+	import MenuBar from './components/Menubar/index.svelte'
 
-    // Dark Mode Config
-  if (
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-  ) {
-      document.documentElement.classList.add('dark');
-  } else {
-      document.documentElement.classList.remove('dark');
-  }
+	// Pages
+	import Login from './pages/login/index.svelte'
+	import SignUp from './pages/signup/index.svelte'
+	import Jokes from './pages/jokes/index.svelte'
+	import Quotes from './pages/quotes/index.svelte'
+	import Goals from './pages/goals/index.svelte'
+	import Journal from './pages/journal/index.svelte'
+	import SingleNote from './pages/SignleNote/index.svelte'
 
-  // Components
-  import Navbar from './components/Nav/index.svelte'
-  import MenuBar from './components/Menubar/index.svelte'
+	// Svelte
+	import { Router, Route } from 'svelte-routing'
 
-  // Svelte
-  import { Router, Route } from 'svelte-routing'
+	// Stores
+	import { MenuBarVisible } from './stores/MenuBarVisble'
+	import { AuthUser } from './stores/User'
 
-  // Stores
-  import { MenuBarVisible } from './stores/MenuBarVisble';
+	let vis = false
+	MenuBarVisible.subscribe((value) => (vis = value))
 
-  let vis = false;
-  MenuBarVisible.subscribe(value => vis = value);
+	let user = {}
+	AuthUser.subscribe((val) => (user = val))
+	console.log(user)
 </script>
 
+<main
+	class="w-full h-full flex flex-col bg-white dark:bg-gray-900 text-black dark:text-white overflow-y-auto"
+>
+	<Navbar />
+	<div class="w-full h-full flex">
+		<!-- {#if vis}
+			<MenuBar />
+		{/if} -->
 
-<main class="w-full h-full flex flex-col bg-white dark:bg-gray-900 text-black dark:text-white">
-  <Navbar />
-  <div class="w-full h-full flex">
-    {#if vis}
-      <MenuBar />
-    {/if}
-
-    <!-- MAIN CONTENT -->
-    <div class="w-full h-full">
-      <Router>
-        <Route>
-          <div class="w-full h-full bg-white center dark:bg-gray-900">
-            Home
-          </div>
-        </Route>
-
-        <Route path="/about">
-            About
-        </Route>
-      </Router>
-    </div>
-  </div>
+		<!-- MAIN CONTENT -->
+		<div class={`w-full h-full ${vis ? "mob:hidden" : ""}`}>
+			<Router>
+				<Route path=""><Jokes /></Route>
+				<Route path="/login"><Login /></Route>
+				<Route path="/signup"><SignUp /></Route>
+				<Route path="/quotes"><Quotes /></Route>
+				<Route path="/goals"><Goals /></Route>
+				<Route path="/journal"><Journal /></Route>
+				<Route path="/journal/:_id" let:params><SingleNote _id={params._id} /></Route>
+				<Route path="/journal/add"><SingleNote type="add" /></Route>
+			</Router>
+		</div>
+	</div>
 </main>
-
